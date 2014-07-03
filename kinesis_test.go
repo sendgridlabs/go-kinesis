809b799a7bd80e9c1e82aba451c447e2133eee26
Peter Heise <info@pheise-it.de>
2014-07-03 14:53:33 +0200

    Reformated code with `gofmt -tabs=false -tabwidth=2 -l -w *.go`

--- kinesis_test.go
+++ kinesis_test.go
@@ -4,21 +4,21 @@ import "testing"
 
 // Trivial test to make sure that Kinesis implements KinesisClient.
 func TestInterfaceIsImplemented(t *testing.T) {
-	var client KinesisClient
-	client = New("BAD_ACCESS_KEY", "BAD_SECRET_KEY", USEast)
-	if client == nil {
-		t.Error("Client is nil")
-	}
+  var client KinesisClient
+  client = New("BAD_ACCESS_KEY", "BAD_SECRET_KEY", USEast)
+  if client == nil {
+    t.Error("Client is nil")
+  }
 }
 
 func TestRegions(t *testing.T) {
-	if EUWest.Name != "eu-west-1" {
-		t.Errorf("%q != %q", EUWest.Name, "eu-west-1")
-	}
-	if USWest2.Name != "us-west-2" {
-		t.Errorf("%q != %q", USWest2.Name, "us-west-2")
-	}
-	if USEast.Name != "us-east-1" {
-		t.Errorf("%q != %q", USEast.Name, "us-east-1")
-	}
+  if EUWest.Name != "eu-west-1" {
+    t.Errorf("%q != %q", EUWest.Name, "eu-west-1")
+  }
+  if USWest2.Name != "us-west-2" {
+    t.Errorf("%q != %q", USWest2.Name, "us-west-2")
+  }
+  if USEast.Name != "us-east-1" {
+    t.Errorf("%q != %q", USEast.Name, "us-east-1")
+  }
 }
