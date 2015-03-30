ca500cb19483aa42898ac80c5b77197f16be5044
Avi Flax <avi@timehop.com>
2015-03-30 17:06:18 -0400

    Make PutRecord example clearer

--- examples/example.go
+++ examples/example.go
@@ -81,7 +81,9 @@ func main() {
 	for i := 0; i < 10; i++ {
 		args = kinesis.NewArgs()
 		args.Add("StreamName", streamName)
-		args.AddRecord([]byte(fmt.Sprintf("Hello AWS Kinesis %d", i)), fmt.Sprintf("partitionKey-%d", i))
+		data := []byte(fmt.Sprintf("Hello AWS Kinesis %d", i))
+		partitionKey := fmt.Sprintf("partitionKey-%d", i)
+		args.AddRecord(data, partitionKey)
 		resp4, err := ksis.PutRecord(args)
 		if err != nil {
 			fmt.Printf("PutRecord err: %v\n", err)
